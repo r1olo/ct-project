38cb835f90ce4922ec331aa19e38097c08e2dbae
Andrea Riolo <andry06082002@gmail.com>
2026-04-28 19:37:19 +0200

    small fix + report

--- src/miniimp/opt.ts
+++ src/miniimp/opt.ts
@@ -28,69 +28,69 @@ function dse(analy: Analysis): boolean {
         if (node.type !== "assign")
             continue;
 
-        /* if the assigned variable in this node IS NOT in the live variables
-         * out set, we can safely delete this node. that means nobody
-         * below us has requested the assigned variable */
+        /* if the assigned variable IS in the live variables out set, we must
+         * NOT delete this node, as it is not useless */
         let liveOut = analy.liveVars.get(node)!.out;
-        if (!liveOut.has(node.ast.i)) {
-            /* we might need a fake skip to avoid having an empty graph */
-            let fakeSkip: ExitNode = { type: "skip" };
+        if (liveOut.has(node.ast.i))
+            continue;
 
-            /* if this is the entry node but has no next, this is the ONLY
-             * node in the graph. therefore, to avoid having an empty graph,
-             * we employ the above fake skip */
-            if (analy.graph.entry === node)
-                analy.graph.entry = node.next ?? fakeSkip;
+        /* we might need a fake skip to avoid having an empty graph */
+        let fakeSkip: ExitNode = { type: "skip" };
 
-            /* we make strong assumptions based on how we structured the graph.
-             * an assign node can't possibly have two predecessors, because
-             * it is not a merge node. moreover, if the predecessor is a
-             * conditional node, we know for sure that there's a merge node (a
-             * fake skip) down the line, so we assume that node.next exists.
-             * for now, just check that we have at most 1 predecessor */
-            let predSet = preds.get(node) ?? new Set();
-            if (predSet.size > 1)
-                throw new RuntimeError("assign node has two predecessors");
-            let pred: Node | undefined = predSet.values().next().value;
+        /* if this is the entry node but has no next, this is the ONLY
+         * node in the graph. therefore, to avoid having an empty graph,
+         * we employ the above fake skip */
+        if (analy.graph.entry === node)
+            analy.graph.entry = node.next ?? fakeSkip;
 
-            /* if the predecessor exists, we check if it's a conditional node
-             * or a normal node. in case of normal node, this might be linked
-             * to an undefined node, ending the graph */
-            if (pred) {
-                if (pred.type === "cond") {
-                    /* for a conditional pred, node.next must be true, because
-                     * the chain must end in a fake skip anyway */
-                    if (node.next === undefined)
-                        throw new RuntimeError("conditional body is malformed");
+        /* we make strong assumptions based on how we structured the graph.
+         * an assign node can't possibly have two predecessors, because
+         * it is not a merge node. moreover, if the predecessor is a
+         * conditional node, we know for sure that there's a merge node (a
+         * fake skip) down the line, so we assume that node.next exists.
+         * for now, just check that we have at most 1 predecessor */
+        let predSet = preds.get(node) ?? new Set();
+        if (predSet.size > 1)
+            throw new RuntimeError("assign node has two predecessors");
+        let pred: Node | undefined = predSet.values().next().value;
 
-                    /* set the next path */
-                    if (pred.true === node)
-                        pred.true = node.next;
-                    else
-                        pred.false = node.next;
-                } else {
-                    /* the standard predecessor of the assign node will either
-                     * point to the next node or end the graph right here */
-                    pred.next = node.next;
+        /* if the predecessor exists, we check if it's a conditional node
+         * or a normal node. in case of normal node, this might be linked
+         * to an undefined node, ending the graph */
+        if (pred) {
+            if (pred.type === "cond") {
+                /* for a conditional pred, node.next must be true, because
+                 * the chain must end in a fake skip anyway */
+                if (node.next === undefined)
+                    throw new RuntimeError("conditional body is malformed");
 
-                    /* if the current node is the exit node, its only
-                     * predecessor is a candidate for being the exit node,
-                     * since it has an undefined next now (TODO: assert or
-                     * trust the code) */
-                    if (analy.graph.exit === node)
-                        analy.graph.exit = pred as ExitNode;
-                }
+                /* set the next path */
+                if (pred.true === node)
+                    pred.true = node.next;
+                else
+                    pred.false = node.next;
             } else {
-                /* with no predecessor, we employ a fake skip if this is the
-                 * last node of the graph */
-                if (analy.graph.exit === node)
-                    analy.graph.exit = fakeSkip;
-            }
+                /* the standard predecessor of the assign node will either
+                 * point to the next node or end the graph right here */
+                pred.next = node.next;
 
-            /* the graph changed here. proceed no further, becuase the
-             * precedessor map is now stale */
-            return true;
+                /* if the current node is the exit node, its only
+                 * predecessor is a candidate for being the exit node,
+                 * since it has an undefined next now (TODO: assert or
+                 * trust the code) */
+                if (analy.graph.exit === node)
+                    analy.graph.exit = pred as ExitNode;
+            }
+        } else {
+            /* with no predecessor, we employ a fake skip if this is the
+             * last node of the graph */
+            if (analy.graph.exit === node)
+                analy.graph.exit = fakeSkip;
         }
+
+        /* the graph changed here. proceed no further, becuase the
+         * precedessor map is now stale */
+        return true;
     }
 
     /* nothing changed, we have an optimized graph */
